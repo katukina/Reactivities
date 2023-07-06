2e28c71d61a9c94504c73c7734d3ecde02a0dad8
Katya Perez Fuster <katukina@hotmail.com>
2023-07-06 14:47:51 +0300

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,9 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//This was added manually after added DataContext class in Persistence
+//KP This was added manually after added DataContext class in Persistence
 builder.Services.AddDbContext<DataContext>(opt => {
    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+//KP Srvice needed to add whcih contains Cors policy to let me access the api from react
+builder.Services.AddCors(opt => {
+    opt.AddPolicy("CorsPolicy", policy => {
+        //KP Alow any http method
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000"); //This has to match where the request is coming, our React app
+    });
 });
 
 var app = builder.Build();
@@ -23,11 +30,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+//KP Before autorization use the one we have created
+app.UseCors("CorsPolicy");
+
 app.UseAuthorization();
 
 app.MapControllers();
 
-//Create DB Get access to DataContext service
+//KP Create DB Get access to DataContext service
 //"using" should be added to automatically cleans up when finished with the code
 using var scope = app.Services.CreateScope();
 var service = scope.ServiceProvider;
